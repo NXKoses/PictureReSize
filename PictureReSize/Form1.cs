447886bf607611492e3d3312ba8f49ba054f461d
NX <nyannkasi@gmail.com>
2024-03-31 21:56:21 +0900

    バージョンテキスト修正

--- PictureReSize/Form1.cs
+++ PictureReSize/Form1.cs
@@ -32,7 +32,7 @@ namespace PictureReSize
             ConvertModeSelect_comboBox.SelectedIndex = 0;
             InputButton.Select();
 
-            this.Text += " ver: 2.0.1.1";
+            this.Text += " ver: 2.0.2.1";
         }
 
         private void InputButton_Click(object sender, EventArgs e)
